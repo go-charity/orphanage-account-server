17b11a016947fe59391ae3788d54c8a615dde55e
onukwilip <onukwilip@gmail.com>
2023-09-14 22:59:57 +0100

    Removed the /dev folder from the SCM

--- dev/routes/edit-orphanage-details.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const edit_orphanage_details_1 = require("../controllers/edit-orphanage-details");
-const editOrphanageDetailsRouter = (0, express_1.Router)();
-editOrphanageDetailsRouter.patch("/", edit_orphanage_details_1.editOrphanageDetails);
-exports.default = editOrphanageDetailsRouter;
